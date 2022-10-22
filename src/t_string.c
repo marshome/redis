f0a2baf229a513d6ae5b56419182cc892464bf50
desertboat <164798848@qq.com>
2022-10-22 16:27:46 +0800

    hash ok

--- src/t_string.c
+++ src/t_string.c
@@ -360,7 +360,6 @@ void setCommandPreprocess(client *c) {
 /* SET key value [NX] [XX] [KEEPTTL] [GET] [EX <seconds>] [PX <milliseconds>]
  *     [EXAT <seconds-timestamp>][PXAT <milliseconds-timestamp>] */
 void setCommand(client *c) {
-    if (c->preprocess.cmd_preprocessed) {
         if (c->preprocess.cmd_stopped) {
             return;
         }
@@ -373,18 +372,6 @@ void setCommand(client *c) {
                                   c->preprocess.set_cmd_unit,
                                   NULL,
                                   NULL);
-    } else {
-        robj *expire = NULL;
-        int unit = UNIT_SECONDS;
-        int flags = OBJ_NO_FLAGS;
-
-        if (parseExtendedStringArgumentsOrReply(c, &flags, &unit, &expire, COMMAND_SET) != C_OK) {
-            return;
-        }
-
-        c->argv[2] = tryObjectEncoding(c->argv[2]);
-        setGenericCommand(c, flags, c->argv[1], c->argv[2], expire, unit, NULL, NULL);
-    }
 }
 
 void setnxCommandPreprocess(client *c) {
